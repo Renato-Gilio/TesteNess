32b9f8b1d345d376064d1475f75529b98e96a702
Renato Augusto ✔ <blant_08@hotmail.com>
2021-03-30 19:23:05 -0300

    Criação de Projeto Crud Ness

---
+++ CrudNessJogadores/CrudNessJogadores/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CrudNessJogadores.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudNessJogadores.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Times> Times { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=TimesDb;Integrated Security=True");
+        }
+    }
+}
---
+++ CrudNessJogadores/CrudNessJogadores/Models/Times.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudNessJogadores.Models
+{
+    public class Times
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome Jogador")]
+        public string NomeJogador { get; set; }
+
+        [Display(Name = "Posição")]
+        public string Posicao { get; set; }
+
+        public string Time { get; set; }
+
+        public string Cores { get; set; }
+    }
+}
